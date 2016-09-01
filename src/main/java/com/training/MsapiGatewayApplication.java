0b75971473f72f1c5831f356a588b4bd7d8fda65
SunilKumarPeljur <sunilkumar92097@gmail.com>
2016-09-01 16:56:02 +0530

    OAuth Security

--- src/main/java/com/training/MsapiGatewayApplication.java
+++ src/main/java/com/training/MsapiGatewayApplication.java
@@ -1,12 +1,8 @@
 package com.training;
 
-import javax.servlet.Filter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -16,8 +12,4 @@ public class MsapiGatewayApplication {
 		SpringApplication.run(MsapiGatewayApplication.class, args);
 	}
 	
-	@Bean
-	public Filter shallowEtagHeaderFilter() {
-		return new ShallowEtagHeaderFilter();
-	}
 }
